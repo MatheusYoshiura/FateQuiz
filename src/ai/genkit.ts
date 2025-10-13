d2627a63ac01e81e1904a534bb04cf8838f5cd24
Matheus Yoshiura <matheusyoshiura17@gmail.com>
2025-10-13 00:21:04 +0000

    Try fixing this error: `Runtime Error: Error: A "use server" file can on

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,6 +1,11 @@
-'use server';
 import {genkit, GenkitError} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
+
+if (!process.env.GEMINI_API_KEY) {
+  throw new GenkitError(
+    'A chave de API do Gemini não foi encontrada. Defina a variável de ambiente GEMINI_API_KEY.'
+  );
+}
 
 export const ai = genkit({
   plugins: [
