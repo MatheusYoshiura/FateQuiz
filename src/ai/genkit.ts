cc7e991edb855be6fe0ac84b8f893fe7d01b9121
Matheus Yoshiura <matheusyoshiura17@gmail.com>
2025-10-13 00:20:48 +0000

    está com erro

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,8 +1,12 @@
 'use server';
-import {genkit} from 'genkit';
+import {genkit, GenkitError} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI({apiKey: process.env.GEMINI_API_KEY})],
+  plugins: [
+    googleAI({
+      apiKey: process.env.GEMINI_API_KEY,
+    }),
+  ],
   model: 'googleai/gemini-2.5-flash',
 });
